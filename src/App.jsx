9a8ee28e69a3189b1f97f37fd75ab55e5ad0e4ac
RitwikaBanerjee2005 <banerjeeritwika4@gmail.com>
2026-08-10 21:51:08 +0530

    Register frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Auth/login";
-
+import Register from "./pages/Auth/register";
 function App() {
 
     return (
@@ -13,6 +13,11 @@ function App() {
                 <Route
                     path="/login"
                     element={<Login />}
+                />
+
+                <Route
+                    path="/register"
+                    element={<Register />}
                 />
 
             </Routes>
